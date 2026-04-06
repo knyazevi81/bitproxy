e4ab673ae882cbbab181e3e116fe2c8e52ed7911
Khasan <khasanknyazev81@gmail.com>
2026-04-06 20:42:09 +0300

    test

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -7,4 +7,4 @@ set -e
 
 echo "Запускается приложение..."
 # exec uvicorn app.presentation.fastapi.main:app --host 0.0.0.0 --port 8000
-exec uvicorn presentation.main:app --host 0.0.0.0 --port 8000
+# exec uvicorn presentation.main:app --host 0.0.0.0 --port 8000
